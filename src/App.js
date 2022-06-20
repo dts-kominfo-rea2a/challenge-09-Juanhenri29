794ff6a697e02f72f2ad3ac6df86834a7f1b8bf8
Juan Sianturi <lioneljuansianturi29@gmail.com>
2022-06-20 15:06:39 +0700

    feat: challenge selesai dikerjakan

--- src/App.js
+++ src/App.js
@@ -11,12 +11,40 @@ const App = () => {
     <div className="App">
       <Header />
       <Contact
-        data1={contacts[0]}
-        data2={contacts[1]}
-        data3={contacts[2]}
-        data4={contacts[3]}
-        data5={contacts[4]}
-        data6={contacts[5]}
+        name={contacts[0].name}
+        email={contacts[0].email}
+        phone={contacts[0].phone}
+        photo={contacts[0].photo}
+      />
+      <Contact
+        name={contacts[1].name}
+        email={contacts[1].email}
+        phone={contacts[1].phone}
+        photo={contacts[1].photo}
+      />
+      <Contact
+        name={contacts[2].name}
+        email={contacts[2].email}
+        phone={contacts[2].phone}
+        photo={contacts[2].photo}
+      />
+      <Contact
+        name={contacts[3].name}
+        email={contacts[3].email}
+        phone={contacts[3].phone}
+        photo={contacts[3].photo}
+      />
+      <Contact
+        name={contacts[4].name}
+        email={contacts[4].email}
+        phone={contacts[4].phone}
+        photo={contacts[4].photo}
+      />
+      <Contact
+        name={contacts[5].name}
+        email={contacts[5].email}
+        phone={contacts[5].phone}
+        photo={contacts[5].photo}
       />
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,33 +2,13 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from "react";
 
-const Contact = ({ data1, data2, data3, data4, data5, data6 }) => {
+const Contact = ({ name, email, phone, photo }) => {
   return (
     <div className="contact">
-      <h3>{data1.name}</h3>
-      <p>{data1.email}</p>
-      <p>{data1.phone}</p>
-      <img src={data1.photo} alt="Foto Kucing"></img>
-      <h3>{data2.name}</h3>
-      <p>{data2.email}</p>
-      <p>{data2.phone}</p>
-      <img src={data2.photo} alt="Foto Kucing"></img>
-      <h3>{data3.name}</h3>
-      <p>{data3.email}</p>
-      <p>{data3.phone}</p>
-      <img src={data3.photo} alt="Foto Kucing"></img>
-      <h3>{data4.name}</h3>
-      <p>{data4.email}</p>
-      <p>{data4.phone}</p>
-      <img src={data4.photo} alt="Foto Kucing"></img>
-      <h3>{data5.name}</h3>
-      <p>{data5.email}</p>
-      <p>{data5.phone}</p>
-      <img src={data5.photo} alt="Foto Kucing"></img>
-      <h3>{data6.name}</h3>
-      <p>{data6.email}</p>
-      <p>{data6.phone}</p>
-      <img src={data6.photo} alt="Foto Kucing"></img>
+      <h3>{name}</h3>
+      <p>{email}</p>
+      <p>{phone}</p>
+      <img src={photo} alt="Foto Kucing"></img>
     </div>
   );
 };
